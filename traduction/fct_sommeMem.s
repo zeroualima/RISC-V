1e548a2146ed1da1b32c3e71c59c2c003f1c8a4f
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-01-17 22:32:48 +0100

    sommeMem termine

--- traduction/fct_sommeMem.s
+++ traduction/fct_sommeMem.s
@@ -15,14 +15,31 @@ void sommeMem(void)
     .globl sommeMem, entry
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    sommeMem : feuille
 Contexte :
-    À compléter
+    res : registre t0
+    i : registre t1
 FIN DU CONTEXTE */
 entry:
 sommeMem:
 sommeMem_fin_prologue:
-/* A compléter */
+    la t0, res # t0 = &res
+    li t2, 0
+    sd t2, 0(t0) # res = 0
+    li t1, 1
+    li t3, 10
+    /* for (i = 1; i <= 10; i++) { */
+loop:
+    blt t3, t1, endloop
+    /* res = res + i; */
+    ld t2, 0(t0) # t2 = *x 
+    add t2, t2, t1 # t2 = t2 + i 
+    sd t2, 0(t0) # *x = t2  
+    addi t1, t1, 1
+    j loop
+endloop:
+    ld t2, 0(t0)
+    mv a0, t2
 sommeMem_debut_epilogue:
     /* éteindre la machine virtuelle QEMU. */
 eteindre_qemu:
@@ -31,9 +48,10 @@ eteindre_qemu:
     sw a1,0(a0)
     ret # pour l'infrastructure d'évaluation automatique
 
-
     .data
-    .globl res
+    .weak res
+    res :
+        .zero 8
 /* uint64_t res;
   La variable globale res étant définie dans ce fichier, il est nécessaire de
   la définir dans la section .data du programme assembleur.
