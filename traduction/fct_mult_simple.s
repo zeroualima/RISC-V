e26adf5d12cdd7bc186aa04350cd4b1bb0da5361
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-01-18 11:13:15 +0100

    mult_simple termine

--- traduction/fct_mult_simple.s
+++ traduction/fct_mult_simple.s
@@ -17,14 +17,42 @@ uint64_t mult_simple(void)
     .globl mult_simple, entry
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    mult_simple : feuille
 Contexte :
-    À compléter
+    x : mémoire
+    y : mémoire
+    res : mémoire
 FIN DU CONTEXTE */
 entry:
 mult_simple:
 mult_simple_fin_prologue:
-/* A compléter */
+    # res = 0;
+    la t0, res # t0 = &res
+    li t1, 0 # t1 = 0
+    sd t1, 0(t0) # *t0 = t1
+# while (y != 0) {
+while:
+    la t0, y # t0 = &y
+    ld t1, 0(t0) # t1 = *t0
+    li t2, 0
+    beq t1, t2, fin_while
+    # res = res + x;
+    la t0, res # t0 = &res
+    ld t1, 0(t0) # t1 = *t0
+    la t2, x # t2 = &x
+    ld t3, 0(t2) # t3 = *t2
+    add t3, t3, t1
+    sd t3, 0(t0)
+    # y--;
+    la t0, y
+    ld t1, 0(t0)
+    addi t1, t1, -1
+    sd t1, 0(t0)
+    j while
+fin_while:
+    la t0, res
+    ld t1, 0(t0)
+    mv a0, t1 
 mult_simple_debut_epilogue:
     /* éteindre la machine virtuelle QEMU. */
 eteindre_qemu:
@@ -39,3 +67,10 @@ eteindre_qemu:
 On utilisera l'attribut .weak (au lieu de .globl) pour éviter des erreurs 
 d'édition de lien si x, y ou res sont déjà définies dans un autre fichier (par exemple decls.c).
 */
+    .weak x, y, res
+    x:
+        .quad 7
+    y:
+        .quad 8
+    res:
+        .quad 42
