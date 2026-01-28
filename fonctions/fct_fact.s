604db414d888d976d8ab79ce178489252ae704ce
Zerouali Mohammed Amine <mohammed-amine.zerouali@grenoble-inp.org>
2026-01-28 15:40:13 +0100

    fact done

--- fonctions/fct_fact.s
+++ fonctions/fct_fact.s
@@ -14,12 +14,30 @@ uint64_t fact(uint64_t n)
     /* uint64_t fact(uint64_t n) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    fact : non feuille
 Contexte :
-    À compléter
+    n : registre a0
 FIN DU CONTEXTE */
 fact:
-/* A compléter */
+    /* on reserve la place nécessaire dans la pile */
+    addi sp, sp, -2*8 # n, ra
 fact_fin_prologue:
+    # Chargement des parametres dans la pile
+    sd ra, 1*8(sp)
+    sd a0, 0*8(sp)
+    li t0, 1
+    sltu t1, t0, a0 /* Le registre t1 est utilisé pour stocker le résultat du test n > 1 */
+    bnez t1, 1f
+    li a0, 1 # return 1;
+    j fact_debut_epilogue
+1:
+    # preparation des arguments
+    addi a0, a0, -1
+    jal fact
+    # ici a0 contient fact(n - 1)
+    ld t0, 0*8(sp) # recupere n avant appel
+    mul a0, a0, t0
 fact_debut_epilogue:
+    ld ra, 1*8(sp)
+    addi sp, sp, 2*8 
     ret
