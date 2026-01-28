f3b25a54979bdaecea68628523a4ae214863cd8f
Zerouali Mohammed Amine <mohammed-amine.zerouali@grenoble-inp.org>
2026-01-28 14:49:10 +0100

    affine done

--- fonctions/fct_affine.s
+++ fonctions/fct_affine.s
@@ -10,12 +10,28 @@ uint64_t affine(uint64_t a, uint64_t b, uint64_t x)
     /* uint64_t affine(uint64_t a, uint64_t b, uint64_t x) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    affine : non feuille
 Contexte :
-    À compléter
+    a : registre a0
+    b : registre a1
+    x : registre a2
 FIN DU CONTEXTE */
 affine:
-/* A compléter */
+  /* on reserve la place nécessaire dans la pile */
+  addi sp, sp, -4*8 # a, b, x, ra
+  sd ra, 4*8(sp)
+  sd a2, 2*8(sp)
+  sd a1, 1*8(sp)
+  sd a0, 0*8(sp)
 affine_fin_prologue:
+    ld a0, 2*8(sp) # preparer x pour mult
+    ld a1, 0*8(sp) # preparer a pour mult
+    jal mult
+    # Ici a0 contient le retour de mult(x, a)
+    ld a1, 1*8(sp) # recupere b
+    # return mult(x, a) + b;
+    add a0, a0, a1 # addition et retour
 affine_debut_epilogue:
+    ld ra, 4*8(sp)
+    addi sp, sp, 4*8
     ret
