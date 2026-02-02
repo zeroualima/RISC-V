a4c59c3b6c7612a26b7c55a3899235591b1da2f3
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-02 12:42:16 +0100

    val_binaire modif

--- fonctions/fct_val_binaire.s
+++ fonctions/fct_val_binaire.s
@@ -29,21 +29,19 @@ Contexte :
     b10 : registre a5
     b9 : registre a6
     b8 : registre a7
-    b7 : pile *(sp+72)
-    b6 : pile *(sp+80)
-    b5 : pile *(sp+88)
-    b4 : pile *(sp+96)
-    b3 : pile *(sp+104)
-    b2 : pile *(sp+112)
-    b1 : pile *(sp+120)
-    b0 : pile *(sp+128)
+    b7 : pile *(sp+0)
+    b6 : pile *(sp+8)
+    b5 : pile *(sp+16)
+    b4 : pile *(sp+24)
+    b3 : pile *(sp+32)
+    b2 : pile *(sp+40)
+    b1 : pile *(sp+48)
+    b0 : pile *(sp+56)
 FIN DU CONTEXTE */
 val_binaire:
-    /* on reserve la place nécessaire dans la pile pour b15, ..., b8 */
-    /* pas de ra car on appelle aucune fonction */
-    addi sp, sp, -8*8
+    /* On ne touche pas a sp, car on ne va rien storer */
 val_binaire_fin_prologue:
-    /* a0, ..., a7 et *(sp+72), ..., *(sp+128) contiennent deja les valeurs des arguments */
+    /* a0, ..., a7 et *(sp+0), ..., *(sp+56) contiennent deja les valeurs des arguments */
     slli a0, a0, 15 # a0 = b15 << 15
     slli a1, a1, 14
     slli a2, a2, 13
@@ -53,13 +51,13 @@ val_binaire_fin_prologue:
     slli a6, a6, 9
     slli a7, a7, 8 # a7 = b8 << 8
 
-    lbu t0, 8*8(sp) # t0 = b7
-    lbu t1, 9*8(sp)
-    lbu t2, 10*8(sp)
-    lbu t3, 11*8(sp)
-    lbu t4, 12*8(sp)
-    lbu t5, 13*8(sp)
-    lbu t6, 14*8(sp) # t6 = b1
+    lbu t0, 0*8(sp) # t0 = b7
+    lbu t1, 1*8(sp)
+    lbu t2, 2*8(sp)
+    lbu t3, 3*8(sp)
+    lbu t4, 4*8(sp)
+    lbu t5, 5*8(sp)
+    lbu t6, 6*8(sp) # t6 = b1
 
     slli t0, t0, 7
     slli t1, t1, 6
@@ -88,5 +86,4 @@ val_binaire_fin_prologue:
     slli t0, t0, 15
     or a0, a0, t0
 val_binaire_debut_epilogue:
-    addi sp, sp, 8*8
     ret
