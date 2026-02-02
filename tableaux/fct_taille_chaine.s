a6e1a2daf7ba7d13299e2c23757afaf4e8a4d64b
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-02 10:50:23 +0100

    taille_chaine done

--- tableaux/fct_taille_chaine.s
+++ tableaux/fct_taille_chaine.s
@@ -23,15 +23,13 @@ taille_chaine:
     addi sp, sp, -2*8
     sd a0, 1*8(sp)
 taille_chaine_fin_prologue:
-    sd zero, 0*8(sp)
+    sd zero, 0*8(sp) # uint64_t taille = 0;
 while: 
     ld t0, 0*8(sp) # t0 = taille
     ld t1, 1*8(sp) # t1 = &chaine 
-    li t2, 8
-    mul t3, t0, t2 # t3 = taille * 8
-    add t4, t1, t3 # t4 = &(chaine + 8 * taille)  
-    lbu t5, 0(t4) # t5 = chaine[taille]
-    beqz t5, fin_while
+    add t2, t1, t0 # t2 = &(chaine + 8 * taille)  
+    lbu t3, 0(t2) # t3 = chaine[taille]
+    beqz t3, fin_while
     addi t0, t0, 1
     sd t0, 0*8(sp)
     j while
