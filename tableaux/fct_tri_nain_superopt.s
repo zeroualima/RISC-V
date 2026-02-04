0bb2cc8758c42079740ba7501fed6fd69d3ecad9
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-04 23:24:01 +0100

    d

--- tableaux/fct_tri_nain_superopt.s
+++ tableaux/fct_tri_nain_superopt.s
@@ -42,29 +42,27 @@ Contexte :
 FIN DU CONTEXTE */
 tri_nain_superopt:
 tri_nain_superopt_fin_prologue:
-    li t0, 0 # t0 = 8 * i
+    mv t0, a0 # t0 = 8 * i + tab
     li t6, 8
     addi t5, a1, -1
-    mul t5, t5, t6 # t5 = 8 * (taille - 1)
+    mul t5, t5, t6 
+    add t5, t5, a0 # t5 = 8 * (taille - 1) + tab
 else1:
     addi t0, t0, 8
 while:
     bge t0, t5, tri_nain_superopt_debut_epilogue
 
-    # Preparation de &tab[i]
-    add t2, a0, t0 # t2 = &tab[i] = tab + 8*i
-
-    ld t3, 0(t2) # t3 = tab[i]
-    ld t4, 1*8(t2) # t4 = tab[i + 1]
+    ld t3, 0(t0) # t3 = tab[i]
+    ld t4, 1*8(t0) # t4 = tab[i + 1]
 
     /* if (tab[i] > tab[i+1]) */
     ble t3, t4, else1
 
-    sd t4, 0(t2) # tab[i] = tab[i + 1]
-    sd t3, 1*8(t2) # tab[i + 1] = tmp
+    sd t4, 0(t0) # tab[i] = tab[i + 1]
+    sd t3, 1*8(t0) # tab[i + 1] = tmp
     
     /* if (i > 0) */
-    blez t0, while
+    ble t0, a0, while
     addi t0, t0, -8
     j while
 tri_nain_superopt_debut_epilogue:
