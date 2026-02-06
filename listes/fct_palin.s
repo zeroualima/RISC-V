1ed556daa80601c3b2ae343853ae7210701e7dcc
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-06 17:21:43 +0100

    palin first try

--- listes/fct_palin.s
+++ listes/fct_palin.s
@@ -17,12 +17,37 @@ bool palin(const char *ch)
     /* bool palin(char *ch) */
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    palin : non feuille
 Contexte :
-    À compléter
+    ra : pile *(sp+1)
+    ch : pile *(sp+0); registre a0
+    inf : registre t0
+    sup : registre t1
 FIN DU CONTEXTE */
 palin:
+    addi sp, sp, -2*8 # ch, ra
+    sd ra, 1*8(sp)
+    sd a0, 0*8(sp)
 palin_fin_prologue:
+    li t0, 0 # inf = 0;
+    jal strlen
+    addi t1, a0, -1 # sup = strlen(ch) - 1;
+while:
+    bge t0, t1, fin_while # condition : inf >= sup
+    ld a0, 0*8(sp) # a0 = ch
+    add t2, a0, t0
+    lbu t2, 0(t2) # t2 = ch[inf]
+    add t3, a0, t1
+    lbu t3, 0(t3) # t3 = ch[sup]
+    bne t2, t3, fin_while
+    addi t0, t0, 1
+    addi t1, t1, -1
+    j while
+fin_while:
+    sltu a0, t0, t1 # contains 1 if inf < sup, and 0 if inf >= sup
+    xori a0, a0, 1
 palin_debut_epilogue:
+    ld ra, 1*8(sp)
+    addi sp, sp, 2*8
     ret
 	.size palin, . - palin
