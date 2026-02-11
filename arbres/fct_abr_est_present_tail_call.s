a9c5fafbf9da78eeca48a1ce0924812b4fc0b163
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-11 10:47:57 +0100

    tail_call first try

--- arbres/fct_abr_est_present_tail_call.s
+++ arbres/fct_abr_est_present_tail_call.s
@@ -16,15 +16,11 @@ bool abr_est_present_tail_call(uint64_t val, struct noeud_t *abr)
     .globl abr_est_present_tail_call
 /* DEBUT DU CONTEXTE
 Fonction :
-    abr_est_present_tail_call : non feuille
+    abr_est_present_tail_call : feuille
 Contexte :
-    ra : pile *(sp+0)
-    abr : registre a1 
     val : registre a0
 FIN DU CONTEXTE */
 abr_est_present_tail_call:
-    addi sp, sp, -1*8
-    sd ra, 0*8(sp)
 abr_est_present_tail_call_fin_prologue:
     bne a1, zero, else_if1
     mv a0, zero
@@ -37,12 +33,9 @@ else_if1:
 else_if2:
     bge a0, t0, else
     ld a1, 1*8(a1) # a1 = abr->fg
-    jal abr_est_present_tail_call
-    j abr_est_present_tail_call_debut_epilogue
+    j abr_est_present_tail_call
 else:
     ld a1, 2*8(a1) # a1 = abr->fd
-    jal abr_est_present_tail_call
+    j abr_est_present_tail_call
 abr_est_present_tail_call_debut_epilogue:
-    ld ra, 0*8(sp)
-    addi sp, sp, 1*8
     ret
