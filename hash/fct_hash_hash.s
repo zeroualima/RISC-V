ce270d39b5db3831779cd58839c9335d02f6e7fd
Mohammed Amine ZEROUALI <mohammed-amine.zerouali@grenoble-inp.org>
2026-02-13 16:50:02 +0100

    first hash

--- hash/fct_hash_hash.s
+++ hash/fct_hash_hash.s
@@ -18,9 +18,11 @@ uint64_t hash_hash(const char *key)
 
 /* DEBUT DU CONTEXTE
 Fonction :
-    nom_de_fonction : feuille ou non feuille
+    hash_hash : feuille
 Contexte :
-    À compléter
+    key : registre a0
+    # hash : registre t0
+    # c : registre t1
 FIN DU CONTEXTE */
 
     .text
@@ -28,6 +30,21 @@ FIN DU CONTEXTE */
     .type  hash_hash, @function
 hash_hash:
 hash_hash_fin_prologue:
+    li t0, 5381
+    lbu t1, 0*8(a0) # uint8_t c = *key;
+while:
+# ATTENTION : *key++ is parsed as *(key++), but it means, derefrence the old pointer, then advance it
+# In brief : c will contain *key, but key will become key++
+# ATTENTION : the condition (c = *key++) is equivalent to ((c = *key++) != 0)
+    lbu t1, 0*8(a0) # t1 = *key
+    addi a0, a0, 8 # a0 = key++
+    beqz t1, fin_while
+    slli t2, t0, 5 # t2 = hash << 5
+    add t2, t2, t0 # t2 = ((hash << 5) + hash)
+    xor t0, t2, t1 # hash = ((hash << 5) + hash) ^ c;
+    j while
+fin_while:
+    mv a0, t0
 hash_hash_debut_epilogue:
     ret
     .size   hash_hash, .-hash_hash
